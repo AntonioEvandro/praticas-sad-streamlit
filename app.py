274e3c587211451218fc77c9e9024f3a095bb25b
Antonio Evandro <100598553+AntonioEvandro@users.noreply.github.com>
2023-05-09 21:50:07 -0300

    pequena adição

--- app.py
+++ app.py
@@ -11,3 +11,8 @@ import pandas as pd
 
 st.title('app')
 
+dataMt = pd.read_csv('sad-01-students-peformace - Math.csv')
+dataPt = pd.read_csv('sad-01-students-peformace - Portuguese.csv')
+
+df = pd.DataFrame(dataPt)
+print(df['age'].describe()['mean'])
