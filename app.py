7c7cf3d506383621e9047f8f4159af91b7e18df1
AntonioEvandro <evandroalves123321@gmail.com>
2023-05-16 11:47:40 -0300

    formatação

--- app.py
+++ app.py
@@ -19,10 +19,48 @@ df = pd.DataFrame(dataPt)
 print("dataPt")
 print(df)
 '''
-escolaGP = df[df['school']=='GP']
-'''
-print("Alunos da escola GP")
-print(escolaGP)
-'''
-print("Media das notas dos alunos da escola GP")
-print(escolaGP['age'].mean())
+def quebLinha():
+    print("")
+
+def questoes(num):
+  if num == 1:
+    escolaGP = df[df['school']=='GP']
+    
+    '''
+    print("Alunos da escola GP")
+    print(escolaGP)
+    media = "{:.2f}".format(escolaGP['age'].mean())
+    '''
+    print("%.2f é a media das notas dos alunos da escola GP" %escolaGP['age'].mean())
+  elif num == 2:
+
+    return 0
+  elif num == 3:
+
+    return 0
+  elif num == 4:
+
+    return 0
+  elif num == 5:
+
+    return 0
+  elif num == 6:
+
+    return 0
+  elif num == 7:
+
+    return 0
+  elif num == 8:
+
+    return 0
+  elif num == 9:
+
+    return 0
+  elif num == 10:
+
+    return 0
+  else:
+    return "Questão não encontrada"
+
+num = int(input('Digite o número da questão que deseja ver o resultado'))
+questoes(num)
